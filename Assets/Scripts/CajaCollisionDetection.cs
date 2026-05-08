9eada14e6a4230cf6cb73ac70c9359e40c6ab8db
Nicolaswid <nicolaswidder@gmail.com>
2026-05-08 14:26:04 -0300

    Initial commit

---
+++ Assets/Scripts/CajaCollisionDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaCollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Colisión con " + col.gameObject.name);
+        if(col.gameObject.CompareTag("Player")){
+            Destroy(col.gameObject);
+        }
+        else if(col.gameObject.CompareTag("Piso")){
+            Destroy(gameObject);
+        }
+    }
+}
